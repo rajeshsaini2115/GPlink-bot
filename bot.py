7937694af08be662dedf93d459cdde1e0d845317
Abir Hasan <hackitabirhasan@gmail.com>
2020-12-18 21:14:26 +0600

    Done

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
-API_KEY = environ.get('API_KEY', '5fd20df0c4db85798dd4f5ff3d03e3606a94f98b')
+API_KEY = 'afe5639a4c191dda12359ed9ebf8b87929a5f428'
 
 bot = Client('gplink bot',
              api_id=API_ID,
