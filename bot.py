93c7a5e568b2d5f9e5a816ce6da9425d5c950de7
Abir Hasan <hackitabirhasan@gmail.com>
2020-12-18 21:45:22 +0600

    Username fixed

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ async def link_handler(bot, message):
         now = datetime.datetime.now()
         chat_id = environ.get('LOG_CHANNEL', -1001283278354)
         uname = f"[{message.from_user.first_name}](tg://openmessage?user_id={message.from_user.id})"
-        await bot.send_message(chat_id, f"**#SHORTEN: \n\n@Convert2GPLink_Bot Shortened** {link} **to** {short_link} **for** {uname} **at** `{now}`", parse_mode="markdown", disable_web_page_preview=True)
+        await bot.send_message(chat_id, f"**#SHORTEN: \n\n@GPLinkz_Bot Shortened** {link} **to** {short_link} **for** {uname} **at** `{now}`", parse_mode="markdown", disable_web_page_preview=True)
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 
